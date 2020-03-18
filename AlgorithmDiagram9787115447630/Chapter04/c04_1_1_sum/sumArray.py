033c01c43051dd8fe2d6894d2daa8464f22b7296
shengexing <937109131@qq.com>
2020-03-18 23:15:24 +0800

    提交内容： 01-4.2 练习题(修改)

--- AlgorithmDiagram9787115447630/Chapter04/c04_1_1_sum/sumArray.py
+++ AlgorithmDiagram9787115447630/Chapter04/c04_1_1_sum/sumArray.py
@@ -4,9 +4,9 @@
 # 对数组中所用元素求和
 def sumArray(num):
     if num:
-        return num.pop() + sumArray(num)
+        return num.pop() + sumArray(num)  # 数组不为空，取出最后一个元素
     else:
-        return 0
+        return 0  # 数组为空，返回 0
 
 
 print(sumArray([1, 2, 3, 4, 5, 6]))
