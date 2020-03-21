90ab7c85417fe3a3d33b198798100c3575d11553
shengexing <937109131@qq.com>
2020-03-21 10:50:56 +0800

    提交内容： 01-4.2 快速排序(随机选择基准值)

--- AlgorithmDiagram9787115447630/Chapter04/c04_2_quickSort/quickSort.py
+++ AlgorithmDiagram9787115447630/Chapter04/c04_2_quickSort/quickSort.py
@@ -1,18 +1,22 @@
 """ 快速排序 """
 
+import random
+
 
 # 快速排序的函数
 def quickSort(array):
     if len(array) < 2:
         return array  # 基线条件：为空或只包含一个元素的数组是 “有序” 的
     else:
-        return quickSort([i for i in array[1:] if i < array[0]]) + \
-               [array[0]] + \
-               quickSort([i for i in array[1:] if i >= array[0]])
+        index = random.randint(0, len(array) - 1)
+        return quickSort(
+            [i for i in array[0:index] + array[index + 1:] if i < array[index]]
+        ) + [array[index]] + quickSort(
+            [i for i in array[0:index] + array[index + 1:] if i >= array[index]]
+        )
 
 
 print(quickSort([]))
 print(quickSort([5]))
 print(quickSort([5, 3]))
 print(quickSort([5, 3, 6, 2, 10]))
-
